6284dd93c04429a31df236d6e52cd3e87a1ecdc0
muhammmadaj12 <104639011+muhammmadaj12@users.noreply.github.com>
2025-05-07 13:23:57 +0500

    First

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,15 +5,12 @@ function updateContainerClass() {
     const width = window.innerWidth;
 
     if (width <= 767) {
-        // For 767px and below, use .container
         container.classList.remove("container-fluid");
         container.classList.add("container");
     } else if (width >= 768 && width <= 991) {
-        // For 768px to 991px, use .container-fluid
         container.classList.remove("container");
         container.classList.add("container-fluid");
     } else {
-        // For 992px and above, use .container
         container.classList.remove("container-fluid");
         container.classList.add("container");
     }
